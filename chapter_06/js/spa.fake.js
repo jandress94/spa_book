24f20978bb0b65e746fcbbef91e20d88369d66c5
Jim <jandress@stanford.edu>
2017-07-11 12:25:19 -0400

    Chpt 6: Added model's avatar functionality

--- chapter_06/js/spa.fake.js
+++ chapter_06/js/spa.fake.js
@@ -61,7 +61,7 @@ spa.fake = (function () {
         };
 
         emit_sio = function (msg_type, data) {
-            var person_map;
+            var person_map, i;
 
             // respond to 'adduser' event with 'userupdate' callback after a 3s delay
             if (msg_type === 'adduser' && callback_map.userupdate) {
@@ -99,6 +99,19 @@ spa.fake = (function () {
                     listchange_idto = undefined;
                 }
                 send_listchange();
+            }
+
+            // simulate send of 'updateavatar' message and data to server
+            if (msg_type === 'updateavatar' && callback_map.listchange) {
+                // simulate receipt of 'listchange' message
+                for (i = 0; i < peopleList.length; i++) {
+                    if (peopleList[i]._id === data.person_id) {
+                        peopleList[i].css_map = data.css_map;
+                        break;
+                    }
+                }
+                // execute callback for the 'listchange' message
+                callback_map.listchange([peopleList]);
             }
         };
 
