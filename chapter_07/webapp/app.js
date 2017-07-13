00e5229cc2ad04a10c0ec6a31a39252dda7540be
Jim <jandress@stanford.edu>
2017-07-13 13:04:37 -0400

    Chpt 7: Middleware using Express, redirect to old SPA so copied in files

--- chapter_07/webapp/app.js
+++ chapter_07/webapp/app.js
@@ -4,14 +4,37 @@
 */
 /*global */
 
-var http, server;
+//------------------------BEGIN MODULE SCOPE VARIABLES----------------------
 
-http = require('http');
-server = http.createServer(function (request, response) {
-    var response_text = request.url === '/test' ? 'you have hit the test page' : 'Hello World!';
-    console.log(request);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(response_text);
-}).listen(3000);
+'use strict';
 
-console.log('Listening on port %d', server.address().port);
+var 
+    http = require('http'),
+    express = require('express'),
+    app = express(),
+    server = http.createServer(app);
+
+//------------------------BEGIN SERVER CONFIGURATION-------------------------
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+});
+app.configure('development', function () {
+    app.use(express.logger());
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
+app.get('/', function (request, response) {
+    response.redirect('/spa.html');
+});
+
+//------------------------BEGIN START SERVER---------------------------------
+server.listen(3000);
+console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
